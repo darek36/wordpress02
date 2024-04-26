fd7497092b343825000187eba8d904e54f0f8038
darek36 <dariusz.c27@gmail.com>
2024-04-26 23:12:31 +0200

    komit 26.04

--- wp-content/themes/pikademia/functions.php
+++ wp-content/themes/pikademia/functions.php
@@ -11,9 +11,18 @@ function theme_setup(){
 
     /** post thumbnail **/
     add_theme_support( 'post-thumbnails' );
-
-   
-
+    add_theme_support( 'custom-background' );
+    add_theme_support( 'custom-logo', array(
+        'height'               => 80,
+        'width'                => 80,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+        'unlink-homepage-logo' => true,
+        ) );
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
+    add_theme_support( 'title-tag' );
+    // add_theme_support( 'custom-logo' );
 
 }
 add_action('after_setup_theme','theme_setup');
